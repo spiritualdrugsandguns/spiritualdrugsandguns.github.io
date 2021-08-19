8b19369c3eb90195c4d23aa095b40cbd04010bf9
spiritualdrugsandguns <moforemmanuel1000@gmail.com>
2021-08-19 12:54:59 +0100

    added second slide

--- js/main.js
+++ js/main.js
@@ -11,40 +11,20 @@ $(document).ready(function(){
         icon.hide();
     });
     
+//slick
+    $('.our-products').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay:true,
+        autoplaySpeed:3000,
+        arrows:true,
+        dots:true
+        
+
+      });
   
 
 
-})
-
-const prev  = document.querySelector('.prev');
-const next = document.querySelector('.next');
-
-const track = document.querySelector('.track');
-
-let carouselWidth = document.querySelector('.carousel-container').offsetWidth;
-
-window.addEventListener('resize', () => {
-  carouselWidth = document.querySelector('.carousel-container').offsetWidth;
-})
-
-let index = 0;
-
-next.addEventListener('click', () => {
-  index++;
-  prev.classList.add('show');
-  track.style.transform = `translateX(-${index * carouselWidth}px)`;
-  
-  if (track.offsetWidth - (index * carouselWidth) < carouselWidth) {
-    next.classList.add('hide');
-  }
-})
-
-prev.addEventListener('click', () => {
-  index--;
-  next.classList.remove('hide');
-  if (index === 0) {
-    prev.classList.remove('show');
-  }
-  track.style.transform = `translateX(-${index * carouselWidth}px)`;
 })
 
